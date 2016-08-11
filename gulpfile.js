58c81685fe806e2bc7203324738ee1923e74e714
John <john@epicodus.com>
2016-08-11 13:41:10 -0700

    get us to the end of initial gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
-gulp.task('myTask', function() {
-  console.log('hello gulp');
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/hockey-interface.js'] })
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
 });
+// $("#target").click(function() {
+//   console.log("clicky clicky");
+// });
