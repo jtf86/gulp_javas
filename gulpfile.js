559bead9a2a558ec4cd82a0df99baa5132b486be
John <john@epicodus.com>
2016-08-17 09:50:23 -0700

    add SASS

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
@@ -51,6 +53,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["scss/*.scss"], ['cssBuild']);
 });
 
 // RUN BOWER INTEGRATION TASKS
@@ -99,6 +102,16 @@ gulp.task("build", function(){
 //REBUILD JS FILES FOR SERVER
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
   browserSync.reload();
+});
+
+//BUILD SASS COMPILE
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
 // REBUILD BOWER FILES FOR SERVER
